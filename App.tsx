719bee7362c96fd9244281f48981841e985dd64d
NΞ◎N <90662438+i-am-neon@users.noreply.github.com>
2024-05-06 19:42:22 -0500

    can send messages to esp32

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import useBLEDevice from './useBLEDevice'; // Custom hook for BLE device management
 import requestBLEPermissions from './requestBLEPermissions';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 const App = () => {
   const [isScanning, setIsScanning] = useState<boolean>(false);
-  const [connected, setConnected] = useState<boolean>(false);
-  const { startScan } = useBLEDevice(setConnected, setIsScanning);
+  const [isConnected, setIsConnected] = useState<boolean>(false);
+  const { startScan, sendBoxValue } = useBLEDevice(setIsConnected, setIsScanning);
+  const [boxvalue, setBoxValue] = useState(false);
 
   useEffect(() => {
     async function setup() {
@@ -24,8 +26,14 @@ const App = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>
-        {isScanning ? "Scanning for devices..." : connected ? "Connected to ESP32" : "Not connected"}
+        {isScanning ? "Scanning for devices..." : isConnected ? "Connected to ESP32" : "Not connected"}
       </Text>
+      <BouncyCheckbox
+        onPress={() => {
+          sendBoxValue(!boxvalue);
+          setBoxValue(!boxvalue);
+        }}
+      />
     </View>
   );
 };
--- useBLEDevice.ts
+++ useBLEDevice.ts
@@ -1,8 +1,109 @@
-import { BleManager } from 'react-native-ble-plx';
-import { useCallback, useEffect, useMemo } from 'react';
+import { BleManager, Device } from 'react-native-ble-plx';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import base64 from 'react-native-base64';
 
-export default function useBLEDevice(setConnected: (connected: boolean) => void, setIsScanning: (scanning: boolean) => void) {
+const SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b';
+
+const MESSAGE_UUID = 'beb5483e-36e1-4688-b7f5-ea07361b26a8';
+
+
+export default function useBLEDevice(setIsConnected: (isConnected: boolean) => void, setIsScanning: (scanning: boolean) => void) {
   const manager = useMemo(() => new BleManager(), []);
+  const [connectedDevice, setConnectedDevice] = useState<Device>();
+
+  async function sendBoxValue(value: boolean) {
+    console.log('sending box value :>> ', value);
+    manager.writeCharacteristicWithResponseForDevice(
+      connectedDevice?.id ?? '',
+      SERVICE_UUID,
+      MESSAGE_UUID,
+      base64.encode(value.toString()),
+    ).then(characteristic => {
+      console.log('Boxvalue changed to :', base64.decode(characteristic.value ?? ''));
+    }).catch(error => {
+      console.error('Error writing characteristic:', error);
+    });
+  }
+
+  const connectDevice = useCallback((device: Device) => {
+    console.log('connecting to Device:', device.name);
+
+    device
+      .connect()
+      .then(device => {
+        setConnectedDevice(device);
+        setIsConnected(true);
+        return device.discoverAllServicesAndCharacteristics();
+      })
+      .then(device => {
+        //  Set what to do when DC is detected
+        manager.onDeviceDisconnected(device.id, (error, device) => {
+          console.log('Device DC');
+          setIsConnected(false);
+        });
+
+        //Read inital values
+
+        //Message
+        device
+          .readCharacteristicForService(SERVICE_UUID, MESSAGE_UUID)
+          .then(valenc => {
+            // setMessage(base64.decode(valenc?.value));
+            console.log('Message:', base64.decode(valenc?.value ?? '') || 'No message found');
+          });
+
+        //BoxValue
+        device
+          .readCharacteristicForService(SERVICE_UUID, BOX_UUID)
+          .then(valenc => {
+            // setBoxValue(StringToBool(base64.decode(valenc?.value)));
+            console.log(
+              'Box Value:',
+              (base64.decode(valenc?.value ?? '')) ||
+              'No box value found',
+            );
+          });
+
+        //monitor values and tell what to do when receiving an update
+
+        //Message
+        device.monitorCharacteristicForService(
+          SERVICE_UUID,
+          MESSAGE_UUID,
+          (error, characteristic) => {
+            if (characteristic?.value != null) {
+              // setMessage(base64.decode(characteristic?.value));
+              console.log('Message update received: ', base64.decode(characteristic?.value));
+              console.log(
+                'Message update received: ',
+                base64.decode(characteristic?.value),
+              );
+            }
+          },
+          'messagetransaction',
+        );
+
+        //BoxValue
+        device.monitorCharacteristicForService(
+          SERVICE_UUID,
+          BOX_UUID,
+          (error, characteristic) => {
+            if (characteristic?.value != null) {
+              // setBoxValue(StringToBool(base64.decode(characteristic?.value)));
+              console.log('Box Value update received: ', base64.decode(characteristic?.value));
+              console.log(
+                'Box Value update received: ',
+                base64.decode(characteristic?.value),
+              );
+            }
+          },
+          'boxtransaction',
+        );
+
+        console.log('Connection established');
+      });
+  }, [manager, setIsConnected]);
+
 
   const startScan = useCallback(() => {
     setIsScanning(true);
@@ -17,11 +118,13 @@ export default function useBLEDevice(setConnected: (connected: boolean) => void,
         console.log(`Found and connecting to device: ${device.name}`);
         manager.stopDeviceScan();
         setIsScanning(false);
-        setConnected(true);
+        setIsConnected(true);
         // Additional connection logic can go here if needed
+        connectDevice(device);
       }
     });
-  }, [manager, setIsScanning, setConnected]);
+  }, [setIsScanning, manager, setIsConnected, connectDevice]);
+
 
   useEffect(() => {
     return () => {
@@ -29,5 +132,5 @@ export default function useBLEDevice(setConnected: (connected: boolean) => void,
     };
   }, [manager]);
 
-  return { startScan };
+  return { startScan, sendBoxValue };
 }
